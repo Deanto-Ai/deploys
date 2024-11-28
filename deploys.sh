b3744b6d666b30bce6ea27fcd47b09444c768741
tonyeandre <tonyeandre@gmail.com>
2024-11-28 07:15:08 -0500

    Update deploys.sh

--- deploys.sh
+++ deploys.sh
@@ -1,3 +1,4 @@
 Application is ready to be deployed to the development environment
 deployement must be reviewed
 development must be reviewed by managers
+Billionaire are being made and I am one of the few trillioniare
